ebf638993d6de4457478a04e8972303ae486bc8d
Eric Bower <me@erock.io>
2022-11-09 14:08:36 -0500

    chore: strip ui elements

--- src/ui/shared/user-menu.tsx
+++ src/ui/shared/user-menu.tsx
@@ -1,38 +1,32 @@
-import { useNavigate } from "react-router";
-
 import { logoutUrl, securitySettingsUrl, sshSettingsUrl } from "@app/routes";
 
 import { useCurrentUser } from "../hooks";
 
 import { tokens } from "./tokens";
-import { Menu, MenuButton, MenuList, MenuItem } from "./menu-button";
+import { Link } from "react-router-dom";
 
 export const UserMenu = () => {
   const { user, isLoading } = useCurrentUser();
-  const navigate = useNavigate();
 
   if (isLoading || !user) {
     return <>Loading...</>;
   }
 
   return (
-    <Menu>
-      <MenuButton>
-        <div className="flex-1">{user.name}</div>
-      </MenuButton>
-      <MenuList>
-        <div className="px-4 py-2">
-          <p className={tokens.type["small semibold darker"]}>{user.name}</p>
-          <p className={tokens.type["small lighter"]}>{user.email}</p>
-        </div>
-        <MenuItem onSelect={() => navigate(sshSettingsUrl())}>
-          SSH Keys
-        </MenuItem>
-        <MenuItem onSelect={() => navigate(securitySettingsUrl())}>
-          Security Settings
-        </MenuItem>
-        <MenuItem onSelect={() => navigate(logoutUrl())}>Logout</MenuItem>
-      </MenuList>
-    </Menu>
+    <div>
+      <div className="px-4 py-2">
+        <p className={tokens.type["small semibold darker"]}>{user.name}</p>
+        <p className={tokens.type["small lighter"]}>{user.email}</p>
+      </div>
+      <div>
+        <Link to={sshSettingsUrl()}>SSH Keys</Link>
+      </div>
+      <div>
+        <Link to={securitySettingsUrl()}>Security Settings</Link>
+      </div>
+      <div>
+        <Link to={logoutUrl()}>Logout</Link>
+      </div>
+    </div>
   );
 };
