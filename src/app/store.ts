b6407474280404fae51e914a4dad0e0a543623f2
Madhu <madhuravius@protonmail.com>
2023-03-01 10:15:54 -0500

    chore: dropped unused import, yikes

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,4 @@
-import { compose, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import type { Middleware, Store } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
