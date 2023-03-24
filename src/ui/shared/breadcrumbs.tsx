49e186c1572aca8f3ff335b2b7605ffaf8aa0d49
Madhu <madhuravius@protonmail.com>
2023-03-23 22:07:27 -0400

    chore: type checking

--- src/ui/shared/breadcrumbs.tsx
+++ src/ui/shared/breadcrumbs.tsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 import { tokens } from "./tokens";
 
 export type Crumb = {
-  name: string | null;
-  to: string;
+  name: string;
+  to: string | null;
 };
 
 const navLink = ({ isActive }: { isActive: boolean }) =>
