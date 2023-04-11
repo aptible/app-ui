e63e9e39625bd671d9dc8793cd5aa08e0c6a9e67
Madhu <madhuravius@protonmail.com>
2023-04-11 00:04:34 -0400

    chore: added saga middleware for sentry

--- src/app/sentry.ts
+++ src/app/sentry.ts
@@ -3,6 +3,14 @@ import * as Sentry from "@sentry/react";
 export const initSentry = () =>
   Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
-    integrations: [new Sentry.BrowserTracing()],
-    tracesSampleRate: 1.0,
+    // WARNING - disabled browser tracing, but this can be enabled for perf tracking
+    integrations: [],
   });
+
+export const sentryErrorReporter = (_: any) => (next: any) => (action: any) => {
+  try {
+    next(action);
+  } catch (err) {
+    Sentry.captureException(err);
+  }
+};
