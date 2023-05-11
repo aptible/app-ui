0aa2ebe5e46e210e8e42f4147b330beef403c95f
Eric Bower <me@erock.io>
2023-05-11 10:00:57 -0400

    tests

--- src/ui/pages/create-project-git-status.test.tsx
+++ src/ui/pages/create-project-git-status.test.tsx
@@ -1,11 +1,22 @@
 import { setupIntegrationTest } from "@app/test";
 import { fireEvent, render, screen } from "@testing-library/react";
 import { CreateProjectGitStatusPage } from "./create-project-git";
-import { server, stacksWithResources, testAccount, testApp } from "@app/mocks";
+import {
+  createId,
+  server,
+  stacksWithResources,
+  testAccount,
+  testApp,
+  testEnv,
+  testServiceRails,
+} from "@app/mocks";
 import {
   createProjectGitStatusUrl,
   CREATE_PROJECT_GIT_STATUS_PATH,
 } from "@app/routes";
+import { rest } from "msw";
+import { defaultEndpointResponse } from "@app/deploy";
+import { defaultHalHref } from "@app/hal";
 
 describe("CreateProjectGitStatusPage", () => {
   describe("when app deployed and no vhost provisioned yet", () => {
@@ -42,6 +53,52 @@ describe("CreateProjectGitStatusPage", () => {
   });
 
   describe("when app deploy and vhost provisioned already", () => {
-    it("should show vhosts and a link to manage them", () => {});
+    it("should show vhosts and a link to manage them", async () => {
+      const handlers = stacksWithResources({
+        accounts: [testAccount],
+        apps: [testApp],
+      });
+      server.use(
+        ...handlers,
+        rest.get(`${testEnv.apiUrl}/apps/:id/vhosts`, (_, res, ctx) => {
+          const jso = {
+            _embedded: {
+              vhosts: [
+                defaultEndpointResponse({
+                  id: createId(),
+                  virtual_domain: "https://some.url",
+                  _links: {
+                    service: defaultHalHref(
+                      `${testEnv.apiUrl}/services/${testServiceRails.id}`,
+                    ),
+                    certificate: defaultHalHref(),
+                  },
+                }),
+              ],
+            },
+          };
+          return res(ctx.json(jso));
+        }),
+      );
+
+      const { TestProvider } = setupIntegrationTest({
+        path: CREATE_PROJECT_GIT_STATUS_PATH,
+        initEntries: [createProjectGitStatusUrl(`${testApp.id}`)],
+      });
+      render(
+        <TestProvider>
+          <CreateProjectGitStatusPage />
+        </TestProvider>,
+      );
+
+      const txt = await screen.findByText("Current Endpoints");
+      expect(txt).toBeInTheDocument();
+      const service = await screen.findByText(testServiceRails.command);
+      expect(service).toBeInTheDocument();
+      const link = await screen.findByRole("link", {
+        name: "Manage Endpoints",
+      });
+      expect(link).toBeInTheDocument();
+    });
   });
 });
