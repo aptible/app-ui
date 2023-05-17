bab5aae1a5ea233b3241f307ce6c27d5d47ecf71
Madhu Ravi <madhuravius@protonmail.com>
2023-05-17 17:53:14 -0400

    chore: added common layout to other pages (#113)
    * chore: added common layout
    
    * chore: updated project create page
    
    * chore: updated error boundary common layout
    
    * chore: temporary commit to avoid lost work
    
    * chore: linted
    
    * chore: updated optional width passage to common bg
    
    * chore: more ui tweaks for non logo wrapper
    
    * chore: optional arg lint

--- src/ui/layouts/hero-bg-layout.tsx
+++ src/ui/layouts/hero-bg-layout.tsx
@@ -1,7 +1,11 @@
 import { AptibleLogo } from "../shared";
 import { Outlet } from "react-router";
 
-export const HeroBgLayout = ({ children }: { children: React.ReactNode }) => {
+export const HeroBgLayout = ({
+  showLogo = true,
+  children,
+  width = 500,
+}: { showLogo?: boolean; children: React.ReactNode; width?: number }) => {
   return (
     <div
       className="flex flex-col flex-1 h-full bg-no-repeat bg-center bg-cover"
@@ -10,15 +14,17 @@ export const HeroBgLayout = ({ children }: { children: React.ReactNode }) => {
       }}
     >
       <main className="flex-1">
-        <div className="py-16">
+        <div className={showLogo ? "py-16" : "py-10"}>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-            <div className="py-4">
+            <div className={showLogo ? "py-4" : "py-0"}>
               <div className="flex justify-center container">
-                <div style={{ width: 500 }}>
-                  <div className="flex items-center justify-center mb-5">
-                    <AptibleLogo width={160} />
-                  </div>
-                  <div className="mt-16">
+                <div style={{ width }}>
+                  {showLogo ? (
+                    <div className="flex items-center justify-center mb-5">
+                      <AptibleLogo width={160} />
+                    </div>
+                  ) : null}
+                  <div className={showLogo ? "mt-16" : "mt-0"}>
                     {children ? children : <Outlet />}
                   </div>
                 </div>
--- src/ui/pages/create-project-git.tsx
+++ src/ui/pages/create-project-git.tsx
@@ -43,6 +43,7 @@ import {
 } from "@app/types";
 import { selectCurrentUser } from "@app/users";
 
+import { HeroBgLayout } from "../layouts";
 import {
   AptibleLogo,
   Banner,
@@ -202,26 +203,9 @@ export const CreateProjectLayout = ({
         </div>
       </div>
 
-      <div
-        className="flex flex-col flex-1 h-full bg-no-repeat bg-center bg-cover"
-        style={{
-          backgroundImage: "url(/background-pattern-v2.png)",
-        }}
-      >
-        <main className="flex-1">
-          <div className="py-6">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-              <div className="py-4">
-                <div className="flex justify-center container">
-                  <div style={{ width: 700 }}>
-                    {children ? children : <Outlet />}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-      </div>
+      <HeroBgLayout showLogo={false} width={700}>
+        {children ? children : <Outlet />}
+      </HeroBgLayout>
     </>
   );
 };
