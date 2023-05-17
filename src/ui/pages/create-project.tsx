73ebdb4f6d49e02075ad98f6604697a5954c4c46
Madhu <madhuravius@protonmail.com>
2023-05-17 15:56:37 -0400

    chore: updated project create page

--- src/ui/pages/create-project.tsx
+++ src/ui/pages/create-project.tsx
@@ -1,5 +1,5 @@
+import { HeroBgLayout } from "../layouts";
 import {
-  AptibleLogo,
   Box,
   Button,
   ButtonIcon,
@@ -258,58 +258,32 @@ export const CreateProjectPage = () => {
   const preDeploySurveyAnswered = useSelector(selectPreDeploySurveyAnswered);
 
   return (
-    <div
-      className="flex flex-col flex-1 h-full bg-no-repeat bg-center bg-cover"
-      style={{
-        backgroundImage: "url(/background-pattern-v2.png)",
-      }}
-    >
-      <main className="flex-1">
-        <div className="py-16">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-            <div className="py-4">
-              <div className="flex justify-center container">
-                <div style={{ width: 500 }}>
-                  <div className="flex items-center justify-center mb-5">
-                    <AptibleLogo width={160} />
-                  </div>
-                  <div className="text-center mt-16">
-                    <h1 className={tokens.type.h1}>Deploy your App</h1>
-                    <p className="my-6 text-gray-600">
-                      Aptible is the{" "}
-                      <span className="text-black font-bold">
-                        No Infrastructure
-                      </span>{" "}
-                      Platform as a Service that startups use to deploy in
-                      seconds, scale infinitely, and forget about
-                      infrastructure.
-                    </p>
-                  </div>
-                  <Box>
-                    <ButtonLink
-                      to={createProjectGitUrl()}
-                      className="font-bold"
-                    >
-                      Deploy with Git Push
-                      <IconArrowRight className="ml-2" />
-                    </ButtonLink>
-                    {!preDeploySurveyAnswered && (
-                      <>
-                        <hr className="h-px mt-8 mb-4 bg-gray-200 border-0 dark:bg-gray-700" />
-                        <FreeFormSurvey />
-                      </>
-                    )}
-                  </Box>
+    <HeroBgLayout>
+      <div className="text-center mt-16">
+        <h1 className={tokens.type.h1}>Deploy your App</h1>
+        <p className="my-6 text-gray-600">
+          Aptible is the{" "}
+          <span className="text-black font-bold">No Infrastructure</span>{" "}
+          Platform as a Service that startups use to deploy in seconds, scale
+          infinitely, and forget about infrastructure.
+        </p>
+      </div>
+      <Box>
+        <ButtonLink to={createProjectGitUrl()} className="font-bold">
+          Deploy with Git Push
+          <IconArrowRight className="ml-2" />
+        </ButtonLink>
+        {!preDeploySurveyAnswered && (
+          <>
+            <hr className="h-px mt-8 mb-4 bg-gray-200 border-0 dark:bg-gray-700" />
+            <FreeFormSurvey />
+          </>
+        )}
+      </Box>
 
-                  <div className="mt-6">
-                    <CreateProjectFooter />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
+      <div className="mt-6">
+        <CreateProjectFooter />
+      </div>
+    </HeroBgLayout>
   );
 };
