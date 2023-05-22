f546f7b2395996d70d30c3bd3cc1361df88c3a12
Eric Bower <me@erock.io>
2023-05-22 16:34:50 -0400

    fix: `afterEach` cleanup for `@testing-library` (#121)

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,7 +1,11 @@
+// https://github.com/vitest-dev/vitest/issues/3077#issuecomment-1484093141
+import "whatwg-fetch";
+
 import { server } from "@app/mocks";
 import matchers, {
   TestingLibraryMatchers,
 } from "@testing-library/jest-dom/matchers";
+import { cleanup } from "@testing-library/react";
 import { expect } from "vitest";
 
 declare module "vitest" {
@@ -13,11 +17,15 @@ declare module "vitest" {
 expect.extend(matchers);
 
 // Establish API mocking before all tests.
-beforeAll(() => server.listen());
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+  server.resetHandlers();
+  // https://testing-library.com/docs/react-testing-library/api/#cleanup
+  cleanup();
+});
 
 // Clean up after the tests are finished.
 afterAll(() => server.close());
