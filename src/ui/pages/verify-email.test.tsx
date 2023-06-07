7ad64ed1ea3ece7cc27c3fcf1d441138d083556a
Madhu <madhuravius@protonmail.com>
2023-06-07 15:24:13 -0400

    chore: pulled out tests to merge

--- src/ui/pages/verify-email.test.tsx
+++ src/ui/pages/verify-email.test.tsx
@@ -2,12 +2,24 @@ import { VerifyEmailPage } from "./verify-email";
 
 import { server, testEnv } from "@app/mocks";
 import { setupIntegrationTest } from "@app/test";
-import { render, screen } from "@testing-library/react";
+import {
+  fireEvent,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { rest } from "msw";
 
 describe("Verify email page", () => {
   it("the verify email page should render", async () => {
-    const { TestProvider } = setupIntegrationTest();
+    const { TestProvider } = setupIntegrationTest({
+      initState: {
+        env: {
+          ...testEnv,
+          origin: "nextgen",
+        },
+      },
+    });
     render(
       <TestProvider>
         <VerifyEmailPage />
@@ -15,23 +27,24 @@ describe("Verify email page", () => {
     );
     await screen.findByRole("button", { name: /Resend Verification Email/ });
   });
-  it("the verify email page should render and redirect if verification succeeds", async () => {
-    const { TestProvider } = setupIntegrationTest();
+  it("the verify email page should render and resend verification successfully", async () => {
+    const { TestProvider } = setupIntegrationTest({
+      initState: {
+        env: {
+          ...testEnv,
+          origin: "nextgen",
+        },
+      },
+    });
     render(
       <TestProvider>
         <VerifyEmailPage />
       </TestProvider>,
     );
-    await screen.findByRole("button", { name: /Resend Verification Email/ });
-  });
-  it("the verify email page should render and remain if verification fails", async () => {
-    const { TestProvider } = setupIntegrationTest();
-    render(
-      <TestProvider>
-        <VerifyEmailPage />
-      </TestProvider>,
-    );
-    await screen.findByRole("button", { name: /Resend Verification Email/ });
+    const el = await screen.findByRole("button", {
+      name: /Resend Verification Email/,
+    });
+    await fireEvent.click(el);
   });
   it("the verify email page should properly fail", async () => {
     const { TestProvider } = setupIntegrationTest();
@@ -48,5 +61,42 @@ describe("Verify email page", () => {
     await screen.findByRole("button", { name: /Resend Verification Email/ });
     const errorText = await screen.queryByText("Failed to verify your email");
     expect(errorText).toBeDefined;
+  });
+  it("the verify email page should render and raise error if resend verification errors", async () => {
+    const { TestProvider } = setupIntegrationTest({
+      initState: {
+        env: {
+          ...testEnv,
+          origin: "nextgen",
+        },
+      },
+    });
+    render(
+      <TestProvider>
+        <VerifyEmailPage />
+      </TestProvider>,
+    );
+    server.use(
+      rest.post(
+        `${testEnv.authUrl}/users/:userId/email_verification_challenges`,
+        (_, res, ctx) => {
+          return res(
+            ctx.status(401),
+            ctx.set("Content-Type", "application/json"),
+            ctx.json({
+              code: 401,
+              exception_context: {},
+              error: "invalid_credentials",
+              message: "mock error message",
+            }),
+          );
+        },
+      ),
+    );
+    const el = await screen.findByRole("button", {
+      name: /Resend Verification Email/,
+    });
+    await fireEvent.click(el);
+    await screen.queryByText("mock error message");
   });
 });
