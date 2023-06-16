a7c7faf79e21c61af2f9981941104333c2ef7394
Madhu <madhuravius@protonmail.com>
2023-06-16 15:02:07 -0400

    chore: updated to conditionally show description if available

--- src/ui/shared/form-group.tsx
+++ src/ui/shared/form-group.tsx
@@ -60,7 +60,9 @@ export function FormGroup({
         <Label htmlFor={htmlFor} {...labelProps}>
           {label}
         </Label>
-        <div className="text-black-500 mb-1">{description}</div>
+        {description ? (
+          <div className="text-black-500 mb-1">{description}</div>
+        ) : null}
       </div>
 
       <div className={`flex ${splitWidthInputs ? "w-1/2" : "flex-col"}`}>
