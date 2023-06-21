9e16fdcdccead9c1c3711a34f7033df32eabc4bf
Eric Bower <me@erock.io>
2023-06-21 16:43:19 -0400

    wrap

--- src/ui/shared/tooltip.tsx
+++ src/ui/shared/tooltip.tsx
@@ -1,5 +1,22 @@
 import cn from "classnames";
 
+/*
+ *.tooltip-inner {
+  display: none;
+  z-index: 1;
+}
+
+.tooltip:hover .tooltip-inner {
+  display: inline;
+}
+
+.tooltip-top {
+  left: 0;
+  top: 0;
+  transform: translate(0%, -110%);
+}
+*/
+
 export const Tooltip = ({
   children,
   text,
@@ -10,13 +27,14 @@ export const Tooltip = ({
       <div
         className={cn([
           "tooltip-inner",
+          "z-50",
+          "left-0 top-0",
           "shadow",
           "absolute",
           "rounded-md",
           "px-3 py-2",
           "max-w-xs",
           "bg-black text-white",
-          "tooltip-top",
         ])}
       >
         {text}
