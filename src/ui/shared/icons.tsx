fbdabb976f3a7d4a90d82e279f7fb704662b3482
Eric Bower <me@erock.io>
2023-06-21 14:32:14 -0400

    fix

--- src/ui/shared/icons.tsx
+++ src/ui/shared/icons.tsx
@@ -349,13 +349,7 @@ export const IconHamburger = (props: Props) => {
 export const IconWorkplace = (props: Props) => {
   return (
     <IconStrokeBase {...props} title="Workplace Icon">
-      <path
-        d="M3.75 21H20.25M4.5 3H19.5M5.25 3V21M18.75 3V21M9 6.75H10.5M9 9.75H10.5M9 12.75H10.5M13.5 6.75H15M13.5 9.75H15M13.5 12.75H15M9 21V17.625C9 17.004 9.504 16.5 10.125 16.5H13.875C14.496 16.5 15 17.004 15 17.625V21"
-        stroke="#0F172A"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+      <path d="M3.75 21H20.25M4.5 3H19.5M5.25 3V21M18.75 3V21M9 6.75H10.5M9 9.75H10.5M9 12.75H10.5M13.5 6.75H15M13.5 9.75H15M13.5 12.75H15M9 21V17.625C9 17.004 9.504 16.5 10.125 16.5H13.875C14.496 16.5 15 17.004 15 17.625V21" />
     </IconStrokeBase>
   );
 };
