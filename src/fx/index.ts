ad41571a62a89337f8f994c8b74068d2922cc48f
Eric Bower <me@erock.io>
2023-06-26 12:40:54 -0400

    refactor: starfx

---
+++ src/fx/index.ts
@@ -0,0 +1,33 @@
+export {
+  selectDataById,
+  defaultLoadingItem,
+  batchActions,
+  resetLoaderById,
+  selectLoaderById,
+  BATCH,
+  prepareStore,
+  call,
+  delay,
+  fetchRetry,
+  poll,
+  put,
+  select,
+  createThrottle,
+  latest,
+  take,
+  fork
+} from "saga-query";
+export type {
+  LoadingState,
+  QueryState,
+  SagaIterator,
+  ApiCtx,
+  Next,
+} from "saga-query";
+export {
+  useApi,
+  useQuery,
+  useCache,
+  useLoader,
+  useLoaderSuccess,
+} from "saga-query/react";
