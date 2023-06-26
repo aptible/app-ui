fa87691d4db114e5255bc898cc80819406573380
Eric Bower <me@erock.io>
2023-06-26 09:12:53 -0400

    lint

--- src/ui/pages/settings.tsx
+++ src/ui/pages/settings.tsx
@@ -3,11 +3,10 @@ import { SettingsSidebar } from "../shared";
 
 export function SettingsPage() {
   return (
-    <MenuWrappedPage
-    >
+    <MenuWrappedPage>
       <div className="flex w-64 flex-col inset-y-0 h-screen">
-          <SettingsSidebar />
-        </div>
+        <SettingsSidebar />
+      </div>
     </MenuWrappedPage>
   );
 }
