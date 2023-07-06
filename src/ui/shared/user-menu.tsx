ac30e076ab66e390a4b116641eb39e5d858b8468
Madhu Ravi <madhuravius@protonmail.com>
2023-07-06 09:32:55 -0400

    chore: added settings link to sbx-main as these panes are not ready (#176)
    * chore: added settings link to sbx-main as these panes are not ready
    
    * chore: linted

--- src/ui/shared/user-menu.tsx
+++ src/ui/shared/user-menu.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 
-import { impersonateUrl, logoutUrl, settingsUrl } from "@app/routes";
+import { impersonateUrl, logoutUrl } from "@app/routes";
 import { selectCanImpersonate } from "@app/users";
 
 import { useCurrentUser } from "../hooks";
@@ -9,9 +9,14 @@ import { IconLogout, IconUserCircle } from "./icons";
 import { IconAlertCircle } from "./icons";
 import { LinkNav } from "./link";
 import { Loading } from "./loading";
+import { selectLegacyDashboardUrl } from "@app/env";
+import { selectOrganizationSelected } from "@app/organizations";
 
 export const UserMenu = ({ hideName = false }: { hideName?: boolean }) => {
   const { user, isLoading } = useCurrentUser();
+  const legacyUrl = useSelector(selectLegacyDashboardUrl);
+  const { id: organizationId } = useSelector(selectOrganizationSelected);
+
   const canImpersonate = useSelector(selectCanImpersonate);
 
   if (isLoading || !user) {
@@ -21,7 +26,7 @@ export const UserMenu = ({ hideName = false }: { hideName?: boolean }) => {
   return (
     <div className="w-full mb-2">
       <LinkNav
-        to={settingsUrl()}
+        to={`${legacyUrl}/organizations/${organizationId}/members`}
         icon={<IconUserCircle />}
         name="Settings"
         hideName={hideName}
