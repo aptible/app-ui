bbaa2c1fb8a50c19bf173e482f56589088d77654
Eric Bower <me@erock.io>
2023-07-18 13:11:26 -0400

    fix: stack img should be part of link

--- src/ui/shared/stack.tsx
+++ src/ui/shared/stack.tsx
@@ -6,20 +6,20 @@ import { Link } from "react-router-dom";
 export const StackItemView = ({ stack }: { stack: DeployStack }) => {
   const stackType = getStackType(stack);
   return (
-    <div className="flex items-center">
-      <img
-        src={
-          stackType === "dedicated"
-            ? "/resource-types/logo-dedicated-stack.png"
-            : "/resource-types/logo-stack.png"
-        }
-        alt="stack icon"
-        className="w-8 h-8 mr-2"
-      />
+    <div>
       <Link
         to={stackDetailEnvsUrl(stack.id)}
-        className="text-black group-hover:text-indigo hover:text-indigo"
+        className="text-black group-hover:text-indigo hover:text-indigo flex items-center"
       >
+        <img
+          src={
+            stackType === "dedicated"
+              ? "/resource-types/logo-dedicated-stack.png"
+              : "/resource-types/logo-stack.png"
+          }
+          alt="stack icon"
+          className="w-8 h-8 mr-2"
+        />
         {stack.name}
       </Link>
     </div>
