4145a33afb3f8cb786a0bf4b9f0b30f756b4ca76
Madhu <madhuravius@protonmail.com>
2023-07-21 16:32:55 -0400

    chore: corrected some scrolling behavior allowed on very large tables

--- src/ui/shared/container-metrics-table.tsx
+++ src/ui/shared/container-metrics-table.tsx
@@ -42,8 +42,8 @@ export const ContainerMetricsDataTable = ({
   }
 
   return (
-    <div className="overflow-x-auto shadow ring-1 ring-black ring-opacity-5 rounded-lg lg:w-full w-[calc(100vw-7.5rem)]">
-      <table className="min-w-full divide-y divide-gray-300">
+    <div className="overflow-x-auto shadow ring-1 ring-black ring-opacity-5 rounded-lg w-[calc(100vw-7.5rem)]">
+      <table className="overflow-x-auto min-w-full divide-y divide-gray-300">
         <TableHead headers={columnHeaders} />
         <tbody className="divide-y divide-gray-200 bg-white">{tableRows}</tbody>
       </table>
