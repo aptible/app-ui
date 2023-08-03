8a292a4e77f9c562edc657c42ff0b9ec459c06c4
Joe Clay <joeclayallday@gmail.com>
2023-08-03 12:11:11 -0500

    Simplify-App-Header-Card
    Remove the word deploy and just show the date for Last Deployed info inside the header card.

--- src/ui/layouts/app-detail-layout.tsx
+++ src/ui/layouts/app-detail-layout.tsx
@@ -58,9 +58,7 @@ export function AppHeader({ app }: { app: DeployApp }) {
         <div className="hidden md:block" />
         <DetailInfoItem title="Last Deployed">
           {app.lastDeployOperation
-            ? `${capitalize(
-                app.lastDeployOperation.type,
-              )} on ${prettyEnglishDate(app.lastDeployOperation?.createdAt)}`
+            ? `${prettyEnglishDate(app.lastDeployOperation?.createdAt)}`
             : "Unknown"}
         </DetailInfoItem>
         <DetailInfoItem title="Docker Image">
