9816d0596775627d7d3f006b686de223c1be6d0f
Joe Clay <joeclayallday@gmail.com>
2023-08-03 13:36:09 -0500

    Revert "Fix Test"
    This reverts commit a30efb3716f8b3eb2acde33e628a864f51174aeb.

--- src/ui/layouts/app-detail-layout.tsx
+++ src/ui/layouts/app-detail-layout.tsx
@@ -19,6 +19,7 @@ import {
   appSettingsUrl,
   environmentAppsUrl,
 } from "@app/routes";
+import { capitalize } from "@app/string-utils";
 import type { AppState, DeployApp } from "@app/types";
 
 import { usePoller } from "../hooks";
