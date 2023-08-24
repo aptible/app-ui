5b27f4806fa4e9e34ac41494a65d60cb5c149cb4
Eric Bower <me@erock.io>
2023-08-24 15:04:41 -0400

    wrap

--- src/app/test/org-picker.test.tsx
+++ src/app/test/org-picker.test.tsx
@@ -176,6 +176,7 @@ describe("Selecting an Organization", () => {
         containers: {},
         databaseImages: {},
         databases: {},
+        databasesCredentials: {},
         endpoints: {},
         environments: {},
         logDrains: {},
