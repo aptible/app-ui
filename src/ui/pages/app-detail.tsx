7d1f1bf1b331bc56c44a6109bd7ca81836afcc1f
Eric Bower <me@erock.io>
2023-08-30 17:43:53 -0400

    fix

--- src/ui/pages/app-detail.tsx
+++ src/ui/pages/app-detail.tsx
@@ -1,7 +1,7 @@
-import { Navigate, useParams } from "react-router"
 import { appServicesUrl } from "@app/routes";
+import { Navigate, useParams } from "react-router";
 
 export const AppDetailPage = () => {
   const { id = "" } = useParams();
-  return <Navigate to={appServicesUrl(id)} replace />
-}
+  return <Navigate to={appServicesUrl(id)} replace />;
+};
--- src/ui/pages/db-detail.tsx
+++ src/ui/pages/db-detail.tsx
@@ -1,7 +1,7 @@
-import { Navigate, useParams } from "react-router"
 import { databaseMetricsUrl } from "@app/routes";
+import { Navigate, useParams } from "react-router";
 
 export const DatabaseDetailPage = () => {
   const { id = "" } = useParams();
-  return <Navigate to={databaseMetricsUrl(id)} replace />
-}
+  return <Navigate to={databaseMetricsUrl(id)} replace />;
+};
