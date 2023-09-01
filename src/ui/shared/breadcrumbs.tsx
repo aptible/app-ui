9b33d4a36a9793fc6d8a486a8f0d9b3bc62590cf
Joe Clay <joeclayallday@gmail.com>
2023-09-01 15:57:55 -0500

    Tweak

--- src/ui/shared/breadcrumbs.tsx
+++ src/ui/shared/breadcrumbs.tsx
@@ -21,14 +21,14 @@ export function Breadcrumbs({ crumbs }: { crumbs: Crumb[] }) {
         {crumbs.map((crumb, idx) => (
           <span key={`${crumb.name}-${idx}`}>
             {crumb.to === null ? (
-              <span className="text-xl font-semibold">{crumb.name}</span>
+              <span className="text-xl font-semibold break-all">{crumb.name}</span>
             ) : (
               <span className={idx === 0 ? "" : ""}>
                 <NavLink className={navLink} to={crumb.to}>
                   {" "}
                   {crumb.name}
                 </NavLink>
-                <span className="text-xl text-gray-500">
+                <span className="text-xl text-gray-500 break-all">
                   {idx !== crumbs.length && " / "}
                 </span>
               </span>
