bd64b32ac6d5705e7f8428d35e18608ab957e4d1
Joe Clay <joeclayallday@gmail.com>
2023-09-05 09:17:40 -0500

    design-tweaks-jc (#349)
    * Fix-Breadcrumbs
    
    * Fix Test
    
    * Tweak
    
    * Fix Test
    
    * Design Tweaks
    
    * Tweak
    
    * Fix URL
    
    * tweaks
    
    * Refactor

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,7 +22,7 @@ module.exports = {
       fontSize: {
         sm: ["12px", "20px"],
         base: ["14px", "20px"],
-        md: ["15px", "24px"],
+        md: ["16px", "24px"],
         lg: ["18px", "24px"],
         xl: ["24px", "32px"],
       },
