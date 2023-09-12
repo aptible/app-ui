04babeacafe5908888384093653c950a20007ab4
Joe Clay <joeclayallday@gmail.com>
2023-09-12 16:03:39 -0500

    add-bg-image

--- src/ui/pages/create-project-name.tsx
+++ src/ui/pages/create-project-name.tsx
@@ -153,6 +153,7 @@ const CreateAppPage = ({ envId }: { envId: string }) => {
           </ButtonCreate>
         </form>
       </Box>
+      <div className="bg-[url('/background-pattern-v2.png')] bg-no-repeat bg-cover absolute w-full h-full top-0 left-0 z-[-999]" />
     </div>
   );
 };
