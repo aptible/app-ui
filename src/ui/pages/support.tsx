22b26a29c32aec5c6f4d2fc1d7992a48496f5223
Graham Michael Rowe <gmrowe@Grahams-MacBook-Pro.local>
2023-09-13 16:46:21 -0400

    fix yarn test lint issue

--- src/ui/pages/support.tsx
+++ src/ui/pages/support.tsx
@@ -366,8 +366,8 @@ export const SupportPage = () => {
                       <p>
                         High
                         <span className="text-sm text-slate-500 ml-2">
-                          (Important functions of your production application are impaired
-                          or degraded.)
+                          (Important functions of your production application
+                          are impaired or degraded.)
                         </span>
                       </p>
                     </div>
@@ -378,7 +378,8 @@ export const SupportPage = () => {
                         Urgent
                         <span className="text-sm text-slate-500 ml-2">
                           (Your business is significantly impacted. Important
-                          functions of your production application are unavailable.)
+                          functions of your production application are
+                          unavailable.)
                         </span>
                       </p>
                     </div>
