623738bef9dc3ba23d662490ff3ec7df5146a927
Joe Clay <joeclayallday@gmail.com>
2023-10-02 23:54:44 -0500

    fix(app): fix statusbox spacing

--- src/ui/shared/status-box.tsx
+++ src/ui/shared/status-box.tsx
@@ -1,6 +1,6 @@
 export const StatusBox = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mb-7">
+    <div className="mt-4 first:mt-0">
       <div className="bg-white pt-4 pb-5 px-5 shadow rounded-lg border border-black-100">
         {children}
       </div>
