ca43c468a98e42e8fe033428f0b15ccd9b9a5a6b
Graham Michael Rowe <gmrowe@Grahams-MacBook-Pro.local>
2023-10-04 15:44:56 -0400

    refactor for tunaEvent

--- src/ui/pages/support.tsx
+++ src/ui/pages/support.tsx
@@ -4,6 +4,7 @@ import {
   uploadAttachment,
 } from "@app/deploy/support";
 import { resetLoaderById, useLoader, useQuery } from "@app/fx";
+import { tunaEvent } from "@app/tuna";
 import { selectCurrentUser } from "@app/users";
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -50,7 +51,7 @@ export const SupportPage = () => {
   });
   const [subjectTyping, setSubjectTyping] = useState(false);
   const [attachedFiles, setAttachedFiles] = useState<AttachmentObject[]>([]);
-  const [viewedSuggestion, setViewedSuggestion] = useState(false)
+  const [viewedSuggestion, setViewedSuggestion] = useState(false);
 
   // Drag and Drop reference hook and functions
   const drop = useRef(null);
@@ -106,13 +107,10 @@ export const SupportPage = () => {
     e.preventDefault();
     const attachments = attachedFiles.map((file) => file.token);
     dispatch(createSupportTicket({ ...formState, attachments: attachments }));
-    
-    const w = window as any;
-    if (w.aptible?.event) {
-        setViewedSuggestion(true)
-        w.aptible.event('submittedAppUiSupportForm', `{ "viewedDocs": "${viewedSuggestion}", "email": "${user.email}" }`);
-    }
-
+    tunaEvent(
+      "submittedAppUiSupportForm",
+      `{ "viewedDocs": "${viewedSuggestion}", "email": "${user.email}" }`,
+    );
     setFormState({
       email: user.email || "",
       name: user.name || "",
@@ -122,7 +120,7 @@ export const SupportPage = () => {
       priority: "",
     });
     setAttachedFiles([]);
-    setViewedSuggestion(false)
+    setViewedSuggestion(false);
   };
 
   const assignAttachment = (attachment: AttachmentObject) => {
@@ -245,13 +243,17 @@ export const SupportPage = () => {
                       return (
                         <li key={key + 1}>
                           <div>
-                            <a target="_blank" href={hit.url} rel="noreferrer" onClick={() => {
-                              const w = window as any;
-                              if (w.aptible?.event) {
-                                  setViewedSuggestion(true)
-                                  w.aptible.event('usedSupportSuggestion', `{ "suggestedUrl": "${hit.url}", "email": "${user.email}" }`);
-                              }
-                            }}>
+                            <a
+                              target="_blank"
+                              href={hit.url}
+                              rel="noreferrer"
+                              onClick={() => {
+                                tunaEvent(
+                                  "usedSupportSuggestion",
+                                  `{ "suggestedUrl": "${hit.url}", "email": "${user.email}" }`,
+                                );
+                              }}
+                            >
                               {hit.title}
                             </a>
                           </div>
