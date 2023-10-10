9131a91c4a5188668cefef03676f288a2247c6e2
Joe Clay <joeclayallday@gmail.com>
2023-10-10 10:26:35 -0500

    style: add certificates icon (#492)

--- src/ui/shared/icons.tsx
+++ src/ui/shared/icons.tsx
@@ -389,3 +389,20 @@ export const IconService = (props: Props) => {
     </IconStrokeBase>
   );
 };
+
+export const IconCertificate = (props: Props) => {
+  return (
+    <IconStrokeBase {...props} title="Service Icon">
+      <path
+        d="M12 15C15.866 15 19 11.866 19 8C19 4.13401 15.866 1 12 1C8.13401 1 5 4.13401 5 8C5 11.866 8.13401 15 12 15Z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8.21 13.89L7 23L12 20L17 23L15.79 13.88"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </IconStrokeBase>
+  );
+};
