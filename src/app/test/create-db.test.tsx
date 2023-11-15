684899f13b760719100b713dfc59fc184900167f
Eric Bower <me@erock.io>
2023-11-15 12:28:13 -0500

    fix(db.create): look for existing db only in selected env (#577)
    When a user tries to create and provision a new database we first
    perform a check to see if a database with that exact name already exists
    within the system.  If it does already exist, we skip creating the
    database and instead move onto provision the database.
    
    We do this because there are cases where a user can create a database
    but the provision operation fails (e.g. user hits plan limits).  When
    the user fixes the issue and returns to FTUX to try to
    reprovision, we would then say validation failed because the database
    already exists.  So if that database already exists we just try to
    reprovision.

--- src/app/test/create-db.test.tsx
+++ src/app/test/create-db.test.tsx
@@ -1,16 +1,22 @@
-import { act, fireEvent, render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { rest } from "msw";
-
+import { defaultDatabaseResponse, defaultOperationResponse } from "@app/deploy";
+import { defaultHalHref } from "@app/hal";
 import {
+  createId,
   server,
   stacksWithResources,
   testAccount,
   testDatabaseOp,
+  testDatabaseServiceId,
+  testDestroyAccount,
+  testDisk,
   testEnv,
+  testPostgresDatabaseImage,
   verifiedUserHandlers,
 } from "@app/mocks";
 import { setupAppIntegrationTest, waitForBootup } from "@app/test";
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { rest } from "msw";
 
 describe("Create Database flow", () => {
   it("should successfully provision a database within an environment", async () => {
@@ -107,5 +113,109 @@ describe("Create Database flow", () => {
       name: /Save/,
     });
     expect(saveBtn).toBeDisabled();
+  });
+
+  describe("when a duplicate db handle already exists in a different environment", () => {
+    it("should create a new database and not try to re-provision that database", async () => {
+      const dupeId = createId();
+      const testDupeDatabaseOp = defaultOperationResponse({
+        id: createId(),
+        type: "provision",
+        status: "succeeded",
+        _links: {
+          resource: defaultHalHref(`${testEnv.apiUrl}/databases/${dupeId}`),
+          account: defaultHalHref(
+            `${testEnv.apiUrl}/accounts/${testDestroyAccount.id}`,
+          ),
+          code_scan_result: defaultHalHref(),
+          self: defaultHalHref(),
+          ssh_portal_connections: defaultHalHref(),
+          ephemeral_sessions: defaultHalHref(),
+          logs: defaultHalHref(),
+          user: defaultHalHref(),
+        },
+      });
+      const dupeDb = defaultDatabaseResponse({
+        id: dupeId,
+        handle: "example-postgres",
+        type: "postgres",
+        connection_url: "postgres://some:val@wow.com:5432",
+        _embedded: {
+          disk: testDisk,
+          last_operation: testDupeDatabaseOp,
+        },
+        _links: {
+          account: defaultHalHref(
+            `${testEnv.apiUrl}/accounts/${testDestroyAccount.id}`,
+          ),
+          initialize_from: defaultHalHref(),
+          database_image: defaultHalHref(
+            `${testEnv.apiUrl}/database_images/${testPostgresDatabaseImage.id}`,
+          ),
+          service: defaultHalHref(
+            `${testEnv.apiUrl}/services/${testDatabaseServiceId}`,
+          ),
+          disk: defaultHalHref(`${testEnv.apiUrl}/disks/${testDisk.id}`),
+        },
+      });
+
+      server.use(
+        ...stacksWithResources({
+          accounts: [testAccount, testDestroyAccount],
+          databases: [dupeDb],
+        }),
+        ...verifiedUserHandlers(),
+        rest.post(
+          `${testEnv.apiUrl}/databases/:id/operations`,
+          async (_, res, ctx) => {
+            return res(ctx.json(testDatabaseOp));
+          },
+        ),
+        rest.get(
+          `${testEnv.apiUrl}/accounts/:envId/operations`,
+          (_, res, ctx) => {
+            return res(
+              ctx.json({
+                _embedded: { operations: [] },
+              }),
+            );
+          },
+        ),
+      );
+
+      const { App, store } = setupAppIntegrationTest({
+        initEntries: [`/create/db?environment_id=${testAccount.id}`],
+      });
+
+      await waitForBootup(store);
+
+      render(<App />);
+
+      await screen.findByText(testAccount.handle);
+      await screen.findByRole("button", { name: /Save/ });
+
+      await screen.findByText(/postgres v14/);
+      const dbSelector = await screen.findByRole("combobox", {
+        name: /new-db/,
+      });
+      await act(() => userEvent.selectOptions(dbSelector, "postgres v14"));
+      const inp = await screen.findByRole("textbox", {
+        name: "dbname",
+      });
+      await act(() => userEvent.clear(inp));
+      await act(() => userEvent.type(inp, "example-postgres"));
+
+      const saveBtn = await screen.findByRole("button", {
+        name: /Save/,
+      });
+
+      // go to next page
+      fireEvent.click(saveBtn);
+
+      // if these two checks fail then that means that an existing database was
+      // re-provisioned in a different environment
+      await screen.findByText(/Operations show real-time/);
+      expect(screen.queryByText(/example-postgres/)).toBeInTheDocument();
+    });
   });
 });
