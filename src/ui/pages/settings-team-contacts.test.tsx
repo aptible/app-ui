3cb645b246b8602f0599c82ae330c55e48279530
Joe Clay <joeclayallday@gmail.com>
2023-11-20 11:54:34 -0600

    Fix Test

--- src/ui/pages/settings-team-contacts.test.tsx
+++ src/ui/pages/settings-team-contacts.test.tsx
@@ -19,15 +19,6 @@ describe("TeamContactsPage", () => {
 
     await screen.findByText(/Organization Settings/);
 
-    const securityEmail = await screen.findByRole("textbox", {
-      name: "security-alert-email",
-    });
-    await act(() => userEvent.type(securityEmail, "team.security@aptible.com"));
-    const opsEmail = await screen.findByRole("textbox", {
-      name: "ops-alert-email",
-    });
-    await act(() => userEvent.type(opsEmail, "team.ops@aptible.com"));
-
     const name = await screen.findByRole("textbox", { name: "org-name" });
     await act(() => userEvent.type(name, "new-org-name"));
     const phone = await screen.findByRole("textbox", { name: "phone" });
@@ -36,15 +27,14 @@ describe("TeamContactsPage", () => {
       name: "emergency-phone",
     });
     await act(() => userEvent.type(emergency, "2222222222"));
-
-    const address = await screen.findByRole("textbox", { name: "address" });
-    await act(() => userEvent.type(address, "123 maple st"));
-    const city = await screen.findByRole("textbox", { name: "city" });
-    await act(() => userEvent.type(city, "san fran"));
-    const state = await screen.findByRole("textbox", { name: "state" });
-    await act(() => userEvent.type(state, "CA"));
-    const zip = await screen.findByRole("textbox", { name: "zip" });
-    await act(() => userEvent.type(zip, "45214"));
+    const securityEmail = await screen.findByRole("textbox", {
+      name: "security-alert-email",
+    });
+    await act(() => userEvent.type(securityEmail, "team.security@aptible.com"));
+    const opsEmail = await screen.findByRole("textbox", {
+      name: "ops-alert-email",
+    });
+    await act(() => userEvent.type(opsEmail, "team.ops@aptible.com"));
 
     const btn = await screen.findByRole("button", { name: /Save/ });
     fireEvent.click(btn);
