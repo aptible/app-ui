117f5f4ae30c7e3c3aedebf857c2c87f9b8a72e5
Eric Bower <me@erock.io>
2023-12-08 14:24:41 -0500

    wrap

--- src/ui/shared/app/services-detail.tsx
+++ src/ui/shared/app/services-detail.tsx
@@ -232,7 +232,7 @@ const ServiceOrgListRow = ({
           {service.appId ? (
             <Link
               to={appDetailUrl(service.appId)}
-              className="text-black group-hover:text-indigo hover:text-indigo w-[130px] text-ellipsis inline-block whitespace-nowrap overflow-x-hidden"
+              className="text-black group-hover:text-indigo hover:text-indigo w-[130px] h-[16px] text-ellipsis inline-block whitespace-nowrap overflow-x-hidden"
             >
               {app.handle}
             </Link>
@@ -240,7 +240,7 @@ const ServiceOrgListRow = ({
           {service.databaseId ? (
             <Link
               to={databaseDetailUrl(service.databaseId)}
-              className="text-black group-hover:text-indigo hover:text-indigo w-[130px] text-ellipsis inline-block whitespace-nowrap overflow-x-hidden"
+              className="text-black group-hover:text-indigo hover:text-indigo w-[130px] h-[16px] text-ellipsis inline-block whitespace-nowrap overflow-x-hidden"
             >
               {db.handle}
             </Link>
