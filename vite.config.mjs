24e1e2b1a7b73979418dcffce9f3486410eba71b
Eric Bower <me@erock.io>
2024-01-03 13:03:43 -0500

    fix: remove luxon from vendor bundle (#672)

--- vite.config.mjs
+++ vite.config.mjs
@@ -28,7 +28,6 @@ export default defineConfig(() => {
               "redux-persist",
               "starfx",
               "qrcode.react",
-              "luxon",
             ],
           },
         },
