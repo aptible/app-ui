ea825ad0364da716c3d4f9e3b8e2f35926e6d6e8
Eric Bower <me@erock.io>
2024-01-07 09:28:13 -0500

    tests

--- src/deploy/configuration/env-vars.test.ts
+++ src/deploy/configuration/env-vars.test.ts
@@ -138,3 +138,117 @@ going and it should work"
     });
   });
 });
+
+// https://github.com/motdotla/dotenv/blob/master/tests/test-parse.js
+describe("dotenv basic", () => {
+  const envBasic = `BASIC=basic
+
+# previous line intentionally left blank
+AFTER_LINE=after_line
+EMPTY=
+EMPTY_SINGLE_QUOTES=''
+EMPTY_DOUBLE_QUOTES=""
+SINGLE_QUOTES='single_quotes'
+SINGLE_QUOTES_SPACED='    single quotes    '
+DOUBLE_QUOTES="double_quotes"
+DOUBLE_QUOTES_SPACED="    double quotes    "
+DOUBLE_QUOTES_INSIDE_SINGLE='double "quotes" work inside single quotes'
+DOUBLE_QUOTES_WITH_NO_SPACE_BRACKET="{ port: $MONGOLAB_PORT}"
+SINGLE_QUOTES_INSIDE_DOUBLE="single 'quotes' work inside double quotes"
+EXPAND_NEWLINES="expand\nnew\nlines"
+DONT_EXPAND_UNQUOTED=dontexpand\nnewlines
+DONT_EXPAND_SQUOTED='dontexpand\nnewlines'
+# COMMENTS=work
+EQUAL_SIGNS=equals==
+RETAIN_INNER_QUOTES={"foo": "bar"}
+RETAIN_INNER_QUOTES_AS_STRING='{"foo": "bar"}'
+TRIM_SPACE_FROM_UNQUOTED=    some spaced out string
+USERNAME=therealnerdybeast@example.tld
+    SPACED_KEY = parsed`;
+
+  it("should match expectations", () => {
+    const envList = configStrToEnvList(envBasic);
+    const env = configEnvListToEnv({}, envList);
+    expect(env.BASIC).toEqual("basic");
+    expect(env.AFTER_LINE).toEqual("after_line");
+    expect(env.EMPTY).toEqual("");
+    expect(env.EMPTY_SINGLE_QUOTES).toEqual("");
+    expect(env.EMPTY_DOUBLE_QUOTES).toEqual("");
+    expect(env.SINGLE_QUOTES).toEqual("single_quotes");
+    expect(env.SINGLE_QUOTES_SPACED).toEqual("    single quotes    ");
+    expect(env.DOUBLE_QUOTES).toEqual("double_quotes");
+    expect(env.DOUBLE_QUOTES_SPACED).toEqual("    double quotes    ");
+    expect(env.DOUBLE_QUOTES_INSIDE_SINGLE).toEqual('double "quotes" work inside single quotes');
+    expect(env.DOUBLE_QUOTES_WITH_NO_SPACE_BRACKET).toEqual('{ port: $MONGOLAB_PORT}');
+    expect(env.SINGLE_QUOTES_INSIDE_DOUBLE).toEqual("single 'quotes' work inside double quotes");
+    expect(env.EXPAND_NEWLINES).toEqual("expand\nnew\nlines");
+    expect(env.DONT_EXPAND_UNQUOTED).toEqual("dontexpand\\nnewlines");
+    expect(env.DONT_EXPAND_SQUOTED).toEqual("dontexpand\\nnewlines");
+    expect(env.EQUAL_SIGNS).toEqual("equals==");
+    expect(env.RETAIN_INNER_QUOTES).toEqual('{"foo": "bar"}');
+    expect(env.RETAIN_INNER_QUOTES_AS_STRING).toEqual('{"foo": "bar"}');
+    expect(env.TRIM_SPACE_FROM_UNQUOTED).toEqual('some spaced out string');
+    expect(env.USERNAME).toEqual('therealnerdybeast@example.tld');
+    expect(env.SPACED_KEY).toEqual('parsed');
+    expect(env.EQUAL_SIGNS).toEqual('equals==');
+    expect(env.RETAIN_INNER_QUOTES).toEqual('{"foo": "bar"}');
+    expect(env.EQUAL_SIGNS).toEqual('equals==');
+    expect(env.RETAIN_INNER_QUOTES).toEqual('{"foo": "bar"}');
+    expect(env.RETAIN_INNER_QUOTES_AS_STRING).toEqual('{"foo": "bar"}');
+    expect(env.TRIM_SPACE_FROM_UNQUOTED).toEqual('some spaced out string');
+    expect(env.USERNAME).toEqual('therealnerdybeast@example.tld');
+    expect(env.SPACED_KEY).toEqual('parsed');
+  });
+});
+
+// https://github.com/motdotla/dotenv/blob/master/tests/test-parse-multiline.js
+describe("dotenv multiline", () => {
+  const envMultiline = `BASIC=basic
+
+# previous line intentionally left blank
+AFTER_LINE=after_line
+EMPTY=
+SINGLE_QUOTES='single_quotes'
+SINGLE_QUOTES_SPACED='    single quotes    '
+DOUBLE_QUOTES="double_quotes"
+DOUBLE_QUOTES_SPACED="    double quotes    "
+EXPAND_NEWLINES="expand\nnew\nlines"
+DONT_EXPAND_UNQUOTED=dontexpand\nnewlines
+DONT_EXPAND_SQUOTED='dontexpand\nnewlines'
+# COMMENTS=work
+EQUAL_SIGNS=equals==
+RETAIN_INNER_QUOTES={"foo": "bar"}
+
+RETAIN_INNER_QUOTES_AS_STRING='{"foo": "bar"}'
+TRIM_SPACE_FROM_UNQUOTED=    some spaced out string
+USERNAME=therealnerdybeast@example.tld
+    SPACED_KEY = parsed
+
+MULTI_DOUBLE_QUOTED="THIS
+IS
+A
+MULTILINE
+STRING"`;
+
+  it("should match expectations", () => {
+    const envList = configStrToEnvList(envMultiline);
+    const env = configEnvListToEnv({}, envList);
+    expect(env.BASIC).toEqual("basic");
+    expect(env.AFTER_LINE).toEqual("after_line");
+    expect(env.EMPTY).toEqual("");
+    expect(env.SINGLE_QUOTES).toEqual("single_quotes");
+    expect(env.SINGLE_QUOTES_SPACED).toEqual("    single quotes    ");
+    expect(env.DOUBLE_QUOTES).toEqual("double_quotes");
+    expect(env.DOUBLE_QUOTES_SPACED).toEqual("    double quotes    ");
+    expect(env.EXPAND_NEWLINES).toEqual("expand\nnew\nlines");
+    expect(env.DONT_EXPAND_UNQUOTED).toEqual("dontexpand\\nnewlines");
+    expect(env.DONT_EXPAND_SQUOTED).toEqual("dontexpand\\nnewlines");
+    expect(env.EQUAL_SIGNS).toEqual("equals==");
+    expect(env.RETAIN_INNER_QUOTES).toEqual('{"foo": "bar"}');
+    expect(env.RETAIN_INNER_QUOTES_AS_STRING).toEqual('{"foo": "bar"}');
+    expect(env.TRIM_SPACE_FROM_UNQUOTED).toEqual('some spaced out string');
+    expect(env.USERNAME).toEqual('therealnerdybeast@example.tld');
+    expect(env.SPACED_KEY).toEqual('parsed');
+    expect(env.MULTI_DOUBLE_QUOTED).toEqual('THIS\nIS\nA\nMULTILINE\nSTRING');
+  });
+});
