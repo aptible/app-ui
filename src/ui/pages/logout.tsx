ce2ec0b7075e610251d3e6080499e9572fdb5353
Joe Clay <joeclayallday@gmail.com>
2024-01-25 15:26:17 -0600

    fix: logout link url (#703)
    * fix: logout link url
    
    * Fix Test
    
    * Fix Tests

--- src/ui/pages/logout.tsx
+++ src/ui/pages/logout.tsx
@@ -1,15 +1,14 @@
 import { logout } from "@app/auth";
-import { selectLegacyDashboardUrl } from "@app/config";
-import { useDispatch, useSelector } from "@app/react";
-import { loginUrl } from "@app/routes";
+import { useDispatch } from "@app/react";
+import { loginUrl, securitySettingsUrl } from "@app/routes";
 import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 import { HeroBgLayout } from "../layouts";
 import { Box, Button, IconArrowRight, tokens } from "../shared";
 
 export const LogoutPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const legacyUrl = useSelector(selectLegacyDashboardUrl);
 
   const logoutSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault();
@@ -22,11 +21,9 @@ export const LogoutPage = () => {
       <div className="text-center mt-16">
         <h1 className={`${tokens.type.h1} text-center`}>Confirm Log Out</h1>
         <p className="my-6 text-gray-600">
-          Click to log out to continue or visit{" "}
-          <a href={`${legacyUrl}/settings/protected/admin`}>
-            Security Settings
-          </a>{" "}
-          to log out of all active sessions.
+          Click to log out now or visit{" "}
+          <Link to={securitySettingsUrl()}>Security Settings</Link> to log out
+          of all active sessions.
         </p>
       </div>
       <Box>
