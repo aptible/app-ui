b719a98a53585b62fe26af9fd136aafb83604928
Eric Bower <me@erock.io>
2024-02-22 17:02:50 -0500

    operation detail

--- src/ui/pages/operation-detail.tsx
+++ src/ui/pages/operation-detail.tsx
@@ -4,10 +4,10 @@ import {
   selectOperationById,
 } from "@app/deploy";
 import { useDispatch, useSelector } from "@app/react";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router";
 import { usePoller } from "../hooks";
-import { LogViewer } from "../shared";
+import { Group, LoadingSpinner, LogViewer } from "../shared";
 
 const cancel = cancelOpByIdPoll();
 export const OpDetailPage = () => {
@@ -15,18 +15,29 @@ export const OpDetailPage = () => {
   const dispatch = useDispatch();
   const op = useSelector((s) => selectOperationById(s, { id }));
   const action = useMemo(() => pollOperationById({ id }), [id]);
+  const [waiting, setWaiting] = useState(false);
 
   usePoller({ action, cancel });
 
   useEffect(() => {
+    if (op.status === "queued" || op.status === "running") {
+      setWaiting(true);
+    }
+
     if (op.status === "failed" || op.status === "succeeded") {
       dispatch(cancel);
+      setWaiting(false);
     }
   }, [op.status]);
 
   return (
     <div className="py-2">
-      <LogViewer op={op} />
+      <Group variant="horizontal" size="sm" className="items-center">
+        <LoadingSpinner show={waiting} />
+        <div className="w-full">
+          <LogViewer op={op} />
+        </div>
+      </Group>
     </div>
   );
 };
