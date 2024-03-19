1aabc4152a0e69098ea5d29218910294166754fc
Eric Bower <me@erock.io>
2024-03-19 10:42:09 -0400

    chore: move deployment monitor

--- src/ui/pages/app-detail-deployments.tsx
+++ src/ui/pages/app-detail-deployments.tsx
@@ -4,8 +4,15 @@ import {
   selectDeploymentsByAppId,
 } from "@app/deployment";
 import { useQuery, useSelector } from "@app/react";
-import { useParams } from "react-router-dom";
-import { DeploymentsTable, DetailPageSections } from "../shared";
+import { appDeployResumeUrl } from "@app/routes";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  ActionBar,
+  ButtonCreate,
+  DeploymentsTable,
+  DetailPageSections,
+  Group,
+} from "../shared";
 
 export function AppDetailDeploymentsPage() {
   const { id = "" } = useParams();
@@ -14,10 +21,25 @@ export function AppDetailDeploymentsPage() {
   const deployments = useSelector((s) =>
     selectDeploymentsByAppId(s, { appId: app.id }),
   );
+  const navigate = useNavigate();
+  const onDeploy = () => {
+    navigate(appDeployResumeUrl(app.id));
+  };
 
   return (
     <DetailPageSections>
-      <DeploymentsTable deployments={deployments} app={app} />
+      <Group>
+        <ActionBar>
+          <ButtonCreate
+            className="w-fit"
+            envId={app.environmentId}
+            onClick={onDeploy}
+          >
+            Deployment Monitor
+          </ButtonCreate>
+        </ActionBar>
+        <DeploymentsTable deployments={deployments} app={app} />
+      </Group>
     </DetailPageSections>
   );
 }
