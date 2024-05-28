4e802d591ffe6c48a1ac6f03b756806703c04942
Eric Bower <me@erock.io>
2024-05-28 09:53:54 -0400

    fix: include current release in metrics for apps and dbs (#823)

--- src/deploy/release/selectors.test.ts
+++ src/deploy/release/selectors.test.ts
@@ -3,6 +3,7 @@ import {
   WebState,
   defaultDeployRelease,
   defaultDeployService,
+  initialState,
 } from "@app/schema";
 import { DeployService } from "@app/types";
 import { selectReleasesByServiceAfterDate } from "./index";
@@ -17,80 +18,69 @@ const testService: DeployService = defaultDeployService({
   id: testServiceRails.id.toString(),
 });
 
-type DeepPartial<T> = {
-  [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
-};
+const release1 = defaultDeployRelease({
+  id: `${createId()}`,
+  serviceId: testServiceRails.id.toString(),
+  createdAt: createdAtByHours({ hours: -1 }),
+});
+const release2 = defaultDeployRelease({
+  id: `${createId()}`,
+  serviceId: testServiceRails.id.toString(),
+  createdAt: createdAtByHours({ hours: -3 }),
+});
+const release3 = defaultDeployRelease({
+  id: `${createId()}`,
+  serviceId: testServiceRails.id.toString(),
+  createdAt: createdAtByHours({ hours: -5 }),
+});
 
-const state: DeepPartial<WebState> = {
-  services: { [testService.id]: testService },
-  releases: {},
-};
-
-describe("selectReleasesByServiceAfterDate", () => {
-  const release1 = defaultDeployRelease({
-    id: `${createId()}`,
-    serviceId: testServiceRails.id.toString(),
-    createdAt: createdAtByHours({ hours: -1 }),
-  });
-  const release2 = defaultDeployRelease({
-    id: `${createId()}`,
-    serviceId: testServiceRails.id.toString(),
-    createdAt: createdAtByHours({ hours: -3 }),
-  });
-  const release3 = defaultDeployRelease({
-    id: `${createId()}`,
-    serviceId: testServiceRails.id.toString(),
-    createdAt: createdAtByHours({ hours: -5 }),
-  });
-
-  const releases = {
+const state: WebState = {
+  ...initialState,
+  services: {
+    [testService.id]: { ...testService, currentReleaseId: release3.id },
+  },
+  releases: {
     [release1.id]: release1,
     [release2.id]: release2,
     [release3.id]: release3,
-  };
+  },
+};
+
+describe("selectReleasesByServiceAfterDate", () => {
   describe("when releases are present (createAt before date (deleted), createdAt after date (deleted), createdAt (active))", () => {
     it("should return 3 releases (all three before cutoff)", () => {
       const date = createdAtByHours({ hours: -6 });
-      const actual = selectReleasesByServiceAfterDate(
-        { ...state, releases } as any,
-        {
-          serviceId: testServiceRails.id.toString(),
-          date,
-        },
-      );
+      const actual = selectReleasesByServiceAfterDate(state, {
+        id: testServiceRails.id.toString(),
+        date,
+      });
       expect(actual.length).toBe(3);
     });
+
     it("should return 3 releases (2 out of range of createdAt, but one is overlapping)", () => {
       const date = createdAtByHours({ hours: -4 });
-      const actual = selectReleasesByServiceAfterDate(
-        { ...state, releases } as any,
-        {
-          serviceId: testServiceRails.id.toString(),
-          date,
-        },
-      );
+      const actual = selectReleasesByServiceAfterDate(state, {
+        id: testServiceRails.id.toString(),
+        date,
+      });
       expect(actual.length).toBe(3);
     });
+
     it("should return 2 releases (1 out of range of createdAt, but one is overlapping)", () => {
       const date = createdAtByHours({ hours: -3 });
-      const actual = selectReleasesByServiceAfterDate(
-        { ...state, releases } as any,
-        {
-          serviceId: testServiceRails.id.toString(),
-          date,
-        },
-      );
+      const actual = selectReleasesByServiceAfterDate(state, {
+        id: testServiceRails.id.toString(),
+        date,
+      });
       expect(actual.length).toBe(2);
     });
+
     it("should return 1 release (only current)", () => {
       const date = createdAtByHours({ hours: 1 });
-      const actual = selectReleasesByServiceAfterDate(
-        { ...state, releases } as any,
-        {
-          serviceId: testServiceRails.id.toString(),
-          date,
-        },
-      );
+      const actual = selectReleasesByServiceAfterDate(state, {
+        id: testServiceRails.id.toString(),
+        date,
+      });
       expect(actual.length).toBe(1);
     });
   });
