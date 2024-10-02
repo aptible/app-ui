3aa3b8db8e8d9047fc4b8cdd938a1086a875e429
joeclayallday <joeclayallday@gmail.com>
2024-10-02 12:54:28 -0500

    Refactor

--- src/ui/pages/environment-detail-backups.tsx
+++ src/ui/pages/environment-detail-backups.tsx
@@ -2,7 +2,13 @@ import { fetchDatabasesByEnvId } from "@app/deploy";
 import { useQuery } from "@app/react";
 import { useParams } from "react-router";
 import { usePaginatedBackupsByEnvId } from "../hooks";
-import { BackupRpView, Banner, DatabaseBackupsList, Group } from "../shared";
+import {
+  BackupRpView,
+  Banner,
+  DatabaseBackupsList,
+  Group,
+  tokens,
+} from "../shared";
 
 export const EnvironmentBackupsPage = () => {
   const { id = "" } = useParams();
@@ -12,16 +18,17 @@ export const EnvironmentBackupsPage = () => {
   return (
     <Group>
       <BackupRpView envId={id} />
-
       <Banner variant="info">
         <p>
-          <strong>Backups of Deprovisioned Databases:</strong> Both Final and
-          Manual backups from deleted databases are shown below. Final backups
-          follow the backup retention policy, while Manual backups are retained
-          indefinitely by default. To manage backups for an active database,
-          visit the Backups tab on the database itself.
+          <strong>
+            Both Final and Manual backups from deleted databases are shown
+            below.
+          </strong>{" "}
+          Final backups follow the backup retention policy, while Manual backups
+          are retained indefinitely by default. To manage backups for an active
+          database, visit the Backups tab on the database itself.
         </p>
-        <p className="">
+        <p>
           <strong>Deleting Copies of Backups: </strong>Removing an original
           backup deletes all its copies, but deleting a copy does not affect the
           original —{" "}
@@ -34,7 +41,7 @@ export const EnvironmentBackupsPage = () => {
           </a>
         </p>
       </Banner>
-
+      <h3 className={tokens.type.h3}>Backups of Deprovisioned Databases</h3>
       <DatabaseBackupsList paginated={paginated} showDatabase showFinal />
     </Group>
   );
