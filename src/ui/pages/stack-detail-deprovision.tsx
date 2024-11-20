9c4151ea8e8e12e46bfad519daa25c13385e3376
joeclayallday <joeclayallday@gmail.com>
2024-11-20 12:21:55 -0600

    WIP

--- src/ui/pages/stack-detail-deprovision.tsx
+++ src/ui/pages/stack-detail-deprovision.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
-import { Box, Button, Group } from "../shared";
+import { supportUrl } from "@app/routes";
+import { Box, Button, ButtonLink, ButtonLinkDocs, Group, IconAlertTriangle } from "../shared";
 
 export const StackDetailDeprovisionPage = () => {
   const { id = "" } = useParams();
@@ -8,16 +9,17 @@ export const StackDetailDeprovisionPage = () => {
     <Group size="sm">
       <Box className="mb-4">
         <div className="flex justify-between items-start">
-          <p className="flex mb-4 text-gray-500 text-md">
-            Contact support to edit or add new VPC Peers.
-          </p>
+          <h1 className="text-lg text-red-500 font-semibold flex items-center gap-2 mb-4">
+            <IconAlertTriangle color="#AD1A1A" />
+            Deprovision Stack
+          </h1>
+          <ButtonLinkDocs href="https://www.aptible.com/docs/support" />
         </div>
-        <Link
-          className="hover:no-underline"
-          to="https://www.aptible.com/docs/support"
-        >
-          <Button className="font-semibold">Contact Support</Button>
-        </Link>
+        <div className="mb-4">
+          <p>If you want to deprovision this Dedicated Stack, you must first deprovision its Environments.</p>
+          <p>Then contact support to permanently delete this stack. This action cannot be undone.</p>
+        </div>
+        <ButtonLink className="w-fit font-semibold" to={supportUrl()}>Contact Support</ButtonLink>
       </Box>
     </Group>
   );
