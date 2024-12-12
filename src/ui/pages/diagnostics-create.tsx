d8a4586b6cf6758802aafffb40efe79476662b3a
joeclayallday <joeclayallday@gmail.com>
2024-12-11 19:03:00 -0600

    Form Styling

--- src/ui/pages/diagnostics-create.tsx
+++ src/ui/pages/diagnostics-create.tsx
@@ -1,4 +1,8 @@
-import { diagnosticsCreateUrl, diagnosticsUrl } from "@app/routes";
+import {
+  diagnosticsCreateUrl,
+  diagnosticsDetailUrl,
+  diagnosticsUrl,
+} from "@app/routes";
 import { AppSidebarLayout } from "../layouts";
 import {
   Banner,
@@ -6,14 +10,33 @@ import {
   Box,
   Breadcrumbs,
   Button,
+  ButtonLink,
+  ButtonLinkDocs,
   FormGroup,
+  FormGroupFeedback,
   Group,
   Input,
   Radio,
   RadioGroup,
+  Select,
   TextArea,
   TitleBar,
+  hr,
+  tokens,
 } from "../shared";
+
+const selectOption = (option: SelectOption) => {
+  setChoice(option.value);
+};
+const options: SelectOption[] = Array(8)
+  .fill(0)
+  .map((_, idx) => ({
+    label: "Select",
+    value: "Select",
+  }));
+const selectedOption = [].find(
+  (option: SelectOption) => option.value === choice,
+);
 
 export const DiagnosticsCreatePage = () => {
   return (
@@ -31,7 +54,135 @@ export const DiagnosticsCreatePage = () => {
         ]}
       />
 
-      <Box>dsfad</Box>
+      <Box>
+        <div className="flex justify-between items-start">
+          <h1 className="text-lg text-gray-500 mb-4">Diagnostics Settings</h1>
+          <ButtonLinkDocs href="https://www.aptible.com/docs/managing-databases" />
+        </div>
+        <form>
+          <FormGroup
+            label="Environment"
+            htmlFor="Environment"
+            feedbackVariant="info"
+            className="mb-4"
+          >
+            <Select
+              onSelect={selectOption}
+              value={selectedOption}
+              options={options}
+            />
+          </FormGroup>
+          <FormGroup
+            label="App"
+            htmlFor="App"
+            feedbackVariant="info"
+            className="mb-4"
+          >
+            <Select
+              onSelect={selectOption}
+              value={selectedOption}
+              options={options}
+            />
+          </FormGroup>
+          <FormGroup
+            label="Start Time"
+            htmlFor="App"
+            feedbackVariant="info"
+            className="mb-4"
+          >
+            <div className="flex items-center gap-2">
+              <Select
+                onSelect={selectOption}
+                value={selectedOption}
+                options={options}
+              />
+              <Input
+                className="h-[37px] w-[45px] pr-0"
+                name="app-handle"
+                type="text"
+                onChange={(e) => setFormInput(e.currentTarget.value)}
+                autoComplete="name"
+                data-testid="input-name"
+                id="input-name"
+                placeholder="HH"
+              />
+              <Input
+                className="h-[37px] w-[48px]  pr-0"
+                name="app-handle"
+                type="text"
+                onChange={(e) => setFormInput(e.currentTarget.value)}
+                autoComplete="name"
+                data-testid="input-name"
+                id="input-name"
+                placeholder="MM"
+              />
+              <p>UTC</p>
+            </div>
+          </FormGroup>
+          <FormGroup
+            label="End Time"
+            htmlFor="App"
+            feedbackVariant="info"
+            className="mb-4"
+          >
+            <div className="flex items-center gap-2">
+              <Select
+                onSelect={selectOption}
+                value={selectedOption}
+                options={options}
+              />
+              <Input
+                className="h-[37px] w-[45px] pr-0"
+                name="app-handle"
+                type="text"
+                onChange={(e) => setFormInput(e.currentTarget.value)}
+                autoComplete="name"
+                data-testid="input-name"
+                id="input-name"
+                placeholder="HH"
+              />
+              <Input
+                className="h-[37px] w-[48px]  pr-0"
+                name="app-handle"
+                type="text"
+                onChange={(e) => setFormInput(e.currentTarget.value)}
+                autoComplete="name"
+                data-testid="input-name"
+                id="input-name"
+                placeholder="MM"
+              />
+              <p>UTC</p>
+            </div>
+          </FormGroup>
+          <FormGroup label="Symptoms" htmlFor="Symptoms" feedbackVariant="info">
+            <FormGroupFeedback>
+              A short description of issues (e.g., "my application is suddenly
+              returning a lot of 500 errors")
+            </FormGroupFeedback>
+            <TextArea
+              className={`${tokens.type.textarea} mt-2`}
+              defaultValue="Why is the app API error rate over 50%"
+            />
+            <div className="mt-4">
+              <hr />
+              <div className="flex items-center gap-2 mt-4">
+                <ButtonLink
+                  to={diagnosticsDetailUrl()}
+                  className="w-[200px] flex font-semibold"
+                >
+                  Generate Dashboard
+                </ButtonLink>
+                <Button
+                  className="w-fit ml-2 flex font-semibold"
+                  variant="white"
+                >
+                  Cancel
+                </Button>
+              </div>
+            </div>
+          </FormGroup>
+        </form>
+      </Box>
     </AppSidebarLayout>
   );
 };
