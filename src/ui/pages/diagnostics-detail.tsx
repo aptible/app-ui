347e0db033b94dde9b583173661510e5e0cffc76
joeclayallday <joeclayallday@gmail.com>
2024-12-11 20:19:14 -0600

    Add New Diagnostics button to app and db detail pages

--- src/ui/pages/diagnostics-detail.tsx
+++ src/ui/pages/diagnostics-detail.tsx
@@ -13,6 +13,7 @@ import {
   RadioGroup,
   TextArea,
   TitleBar,
+  tokens,
 } from "../shared";
 
 export const DiagnosticsDetailPage = () => {
@@ -31,7 +32,30 @@ export const DiagnosticsDetailPage = () => {
         ]}
       />
 
-      <Box>dsfad</Box>
+      <div className="flex flex-col gap-4">
+        {/*Widget*/}
+        <div className="bg-white shadow rounded-lg border border-black-100 relative min-h-[100px] max-h-[460px] overflow-hidden">
+          <div className="flex items-center gap-2 py-3 px-4 bg-black-50">
+            <img
+              src="/resource-types/logo-datadog.png"
+              className="w-[20px] h-[20px] mr-1 align-middle"
+              aria-label="App"
+            />
+            <p className="font-semibold pb-1">Chart Label</p>
+          </div>
+        </div>
+        {/*Widget*/}
+        <div className="bg-white shadow rounded-lg border border-black-100 relative min-h-[100px] max-h-[460px] overflow-hidden">
+          <div className="flex items-center gap-2 py-3 px-4 bg-black-50">
+            <img
+              src="/resource-types/logo-datadog.png"
+              className="w-[20px] h-[20px] mr-1 align-middle"
+              aria-label="App"
+            />
+            <p className="font-semibold pb-1">Chart Label</p>
+          </div>
+        </div>
+      </div>
     </AppSidebarLayout>
   );
 };
