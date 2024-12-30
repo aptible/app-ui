d1538e0e9c4b69f466f390fb95d802a038b33112
Eric Abruzzese <eric.abruzzese@aptible.com>
2024-12-30 14:05:13 -0500

    Configure sourcemaps for Sentry (#1015)

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,18 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+
+
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const enableSentrySourcemaps = env.SENTRY_AUTH_TOKEN && env.SENTRY_PROJECT;
+
   return {
     build: {
+      sourcemap: true,
       rollupOptions: {
         output: {
           manualChunks: {
@@ -28,7 +35,21 @@ export default defineConfig(() => {
         },
       },
     },
-    plugins: [react(), tsconfigPaths()],
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      ...(
+        enableSentrySourcemaps
+        ? [
+            sentryVitePlugin({
+              authToken: env.SENTRY_AUTH_TOKEN,
+              org: env.SENTRY_ORG || "aptible",
+              project: env.SENTRY_PROJECT,
+            }),
+          ]
+        : []
+      ),
+    ],
     server: {
       port: 4200,
     },
