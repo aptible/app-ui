5b1cf2e666cb092ab41f6a4eb38c4ef36f1ace2a
joeclayallday <joeclayallday@gmail.com>
2025-01-16 03:15:58 -0600

    Hide operations over time

--- src/ui/pages/dashboard.tsx
+++ src/ui/pages/dashboard.tsx
@@ -42,17 +42,6 @@ export const DashboardPage = () => {
             <div className="flex-1"><div className="text-lg font-semibold">2</div><div>Members</div></div>
           </div>
         </Box>
-        <Box className="flex flex-col !p-0 overflow-hidden">
-          <div className="py-2 px-4 bg-black-50 flex flex-row items-center">
-            <div className="text-md text-gray-500 flex-1">Operations Over Time</div>
-            <div class="flex">
-              <button disabled="" type="button" class="rounded-r-none hover:z-10 pointer-events-none !bg-black-100 flex items-center justify-center px-4 py-[1.5px] text-sm font-medium border border-black-100 font-bold shadow-sm text-black bg-white hover:bg-black-100 hover:border-black-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black-200 rounded-md opacity-50">1H</button>
-              <button type="button" class="rounded-none -ml-[1px] -mr-[1px] hover:z-10  flex items-center justify-center px-4 py-[1.5px] text-sm font-medium border border-black-100 font-bold shadow-sm text-black bg-white hover:bg-black-100 hover:border-black-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black-200 rounded-md">1D</button>
-              <button type="button" class="rounded-l-none hover:z-10  flex items-center justify-center px-4 py-[1.5px] text-sm font-medium border border-black-100 font-bold shadow-sm text-black bg-white hover:bg-black-100 hover:border-black-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black-200 rounded-md">1W</button>
-              </div>
-          </div>
-          <div className="p-4">test</div>
-        </Box>
         <Box>
           <div className="flex flex-row items-center gap-4 mb-3"><img src="/resource-types/logo-database.png" className="w-[30px] h-[30px]"></img><div className="text-xl font-semibold text-black">Introduction to Aptible</div></div>
           <div className="text-md text-black mb-4">Aptible is the No Infrastructure Platform as a Service that startups use to deploy in seconds, scale infinitely, and forget about infrastructure.</div>
