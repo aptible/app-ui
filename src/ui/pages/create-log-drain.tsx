c7fcf9e6be6938d3f1949bd261689d61f32c7ecb
Michael Wang <wang.michael213@gmail.com>
2025-03-03 11:49:58 -0500

    syslog doesn't require token

--- src/ui/pages/create-log-drain.tsx
+++ src/ui/pages/create-log-drain.tsx
@@ -102,9 +102,9 @@ const validators = {
     if (p.url === "") return "Must provide a URL for log drain";
     if (!p.url.startsWith("https")) return "Must begin with https://";
   },
-  // insightops / syslog_tls_tcp
+  // insightops
   loggingToken: (p: CreateLogDrainProps) => {
-    if (!(p.drainType === "insightops" || p.drainType === "syslog_tls_tcp"))
+    if (!(p.drainType === "insightops"))
       return;
     if (p.loggingToken === "") return "Must provide a token for log drain.";
   },
