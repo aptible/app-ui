afab76fb8b6c2261f2d6af0f222352351963006c
Michael Peterson <141289+mjp@users.noreply.github.com>
2025-03-31 08:32:01 -0400

    Fix custom resources pagination (#1070)
    * Load all custom resources & paginate client side
    
    * Fix bug on filtering out self references
    
    * Reset schema when loading custom resources from the server
    
    * Fix formatting
    
    ---------
    
    Co-authored-by: Frank Macreery <frank@aptible.com>

--- src/deploy/custom-resource/index.ts
+++ src/deploy/custom-resource/index.ts
@@ -1,4 +1,4 @@
-import { api } from "@app/api";
+import { api, cacheMinTimer } from "@app/api";
 import { createSelector } from "@app/fx";
 import { defaultEntity, extractIdFromLink } from "@app/hal";
 import { selectOrganizationSelectedId } from "@app/organizations";
@@ -101,7 +101,19 @@ export const selectCustomResourcesByOrgAsList = createSelector(
   },
 );
 
-export const fetchCustomResources = api.get("/custom_resources");
+export const fetchCustomResources = api.get(
+  "/custom_resources?per_page=5000",
+  {
+    supervisor: cacheMinTimer(),
+  },
+  function* (ctx, next) {
+    yield* next();
+    if (!ctx.json.ok) {
+      return;
+    }
+    yield* schema.update(schema.customResources.reset());
+  },
+);
 
 export const fetchCustomResource = api.get<{ id: string }>(
   "/custom_resources/:id",
