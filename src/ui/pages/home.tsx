43a34adb4b1d6fe2a5fd2291a5e87cc37ccb1375
Frank Macreery <frank@aptible.com>
2025-04-10 19:53:46 -0400

    Correctly route to /software-catalog from / if selectHasDiagnosticsPocFeature

--- src/ui/pages/home.tsx
+++ src/ui/pages/home.tsx
@@ -1,13 +1,29 @@
+import { FETCH_REQUIRED_DATA } from "@app/bootup";
 import { selectHasDiagnosticsPocFeature } from "@app/organizations";
 import { useSelector } from "@app/react";
-import { diagnosticsUrl, environmentsUrl } from "@app/routes";
+import { environmentsUrl, softwareCatalogUrl } from "@app/routes";
+import { schema } from "@app/schema";
 import { Navigate } from "react-router";
+import { Loading } from "../shared";
 
 export const HomePage = () => {
+  const loader = useSelector((s) =>
+    schema.loaders.selectById(s, { id: FETCH_REQUIRED_DATA }),
+  );
   const hasDiagnosticsPoc = useSelector(selectHasDiagnosticsPocFeature);
+
+  // Wait for required data to be loaded before redirecting
+  if (loader.status !== "success") {
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        <Loading text="Loading" />
+      </div>
+    );
+  }
+
   return (
     <Navigate
-      to={hasDiagnosticsPoc ? diagnosticsUrl() : environmentsUrl()}
+      to={hasDiagnosticsPoc ? softwareCatalogUrl() : environmentsUrl()}
       replace
     />
   );
