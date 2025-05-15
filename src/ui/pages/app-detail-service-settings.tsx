f8f7a80b81b7d0efa7b56625a54dfeb903a2feff
Ann Guilinger <ann.guilinger@aptible.com>
2025-05-15 11:38:55 -0400

    Allow configuration of stop timeout in service deployment settings (#1105)
    * Allow configuration of stop timeout in service deployment settings
    
    * Add min/max to helper text

--- src/ui/pages/app-detail-service-settings.tsx
+++ src/ui/pages/app-detail-service-settings.tsx
@@ -1,16 +1,13 @@
 import {
   fetchApp,
   fetchService,
-  getEndpointUrl,
   selectEndpointsByServiceId,
   selectServiceById,
   updateServiceById,
 } from "@app/deploy";
 import { useDispatch, useLoader, useQuery, useSelector } from "@app/react";
-import { endpointDetailUrl } from "@app/routes";
 import { type SyntheticEvent, useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { Link } from "react-router-dom";
 import {
   Banner,
   BannerMessages,
@@ -18,6 +15,8 @@ import {
   Button,
   ButtonLinkDocs,
   CheckBox,
+  FormGroup,
+  Input,
   Tooltip,
 } from "../shared";
 
@@ -39,7 +38,10 @@ export const AppDetailServiceSettingsPage = () => {
   const action = updateServiceById({ ...nextService });
   const modifyLoader = useLoader(action);
   const cancelChanges = () => setNextService(service);
-  const changesExist = service !== nextService;
+  const changesExist =
+    service.forceZeroDowntime !== nextService.forceZeroDowntime ||
+    service.naiveHealthCheck !== nextService.naiveHealthCheck ||
+    service.stopTimeout !== nextService.stopTimeout;
 
   const onSubmitForm = (e: SyntheticEvent) => {
     e.preventDefault();
@@ -50,78 +52,103 @@ export const AppDetailServiceSettingsPage = () => {
     <div className="flex flex-col gap-4">
       <Box>
         <form onSubmit={onSubmitForm}>
-          <BannerMessages {...modifyLoader} />
+          <BannerMessages {...modifyLoader} className="mb-4" />
           <div className="flex flex-col gap-2">
             <div className="flex justify-between items-start">
-              <h1 className="text-lg text-gray-500 mb-4">Service Settings</h1>
+              <h1 className="text-lg text-gray-500 mb-4">
+                Service Deployment Settings
+              </h1>
               <ButtonLinkDocs href="https://www.aptible.com/docs/core-concepts/apps/deploying-apps/releases/overview" />
             </div>
-            {endpoints.length > 0 ? (
-              <Banner>
-                Service settings are managed through the following Endpoints:
-                {endpoints.map((endpoint, index) => {
-                  return (
-                    <span key={index}>
-                      {index === 0 && " "}
-                      <Link to={endpointDetailUrl(endpoint.id)}>
-                        {getEndpointUrl(endpoint)}
-                      </Link>
-                      {index < endpoints.length - 1 && ", "}
-                    </span>
-                  );
-                })}
-              </Banner>
-            ) : (
-              <>
-                <h2 className="text-md font-semibold">Deployment Strategy</h2>
-                <CheckBox
-                  name="zero-downtime"
-                  label="Enable Zero-Downtime Deployment"
-                  checked={nextService.forceZeroDowntime}
-                  onChange={(e) =>
-                    setNextService({
-                      ...nextService,
-                      forceZeroDowntime: e.currentTarget.checked,
-                    })
-                  }
-                />
-                <div className="flex gap-2">
-                  <Tooltip text="When enabled, ignores Docker healthchecks and instead only waits to ensure the container stays up for at least 30 seconds.">
-                    <CheckBox
-                      name="simple-healthcheck"
-                      label="Use simple healthcheck (30s)"
-                      checked={nextService.naiveHealthCheck}
-                      onChange={(e) =>
+            <div className="flex flex-col gap-2">
+              {endpoints.length > 0 ? (
+                <Banner>
+                  Cannot configure zero-downtime or non-endpoint health checks
+                  for services with endpoints.
+                </Banner>
+              ) : null}
+              {endpoints.length === 0 && (
+                <>
+                  <CheckBox
+                    name="zero-downtime"
+                    label="Enable Zero-Downtime Deployment"
+                    checked={nextService.forceZeroDowntime}
+                    onChange={(e) =>
+                      setNextService({
+                        ...nextService,
+                        forceZeroDowntime: e.currentTarget.checked,
+                      })
+                    }
+                  />
+                  <div className="flex gap-2">
+                    <Tooltip text="When enabled, ignores Docker healthchecks and instead only waits to ensure the container stays up for at least 30 seconds.">
+                      <CheckBox
+                        name="simple-healthcheck"
+                        label="Use simple healthcheck (30s)"
+                        checked={nextService.naiveHealthCheck}
+                        onChange={(e) =>
+                          setNextService({
+                            ...nextService,
+                            naiveHealthCheck: e.currentTarget.checked,
+                          })
+                        }
+                      />
+                    </Tooltip>
+                  </div>
+                </>
+              )}
+              <div className="flex flex-col gap-4 items-center mb-4">
+                <div className="flex flex-col w-full gap-4">
+                  <FormGroup
+                    splitWidthInputs
+                    description={`The time in seconds to wait for the old release's containers to stop before killing them. Must be between 1 and 900 seconds.`}
+                    label="Stop Timeout"
+                    htmlFor="stop-timeout"
+                    className="mt-4"
+                  >
+                    <Input
+                      id="stop-timeout"
+                      type="number"
+                      min={0}
+                      max={900}
+                      value={nextService.stopTimeout ?? ""}
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        const parsedValue =
+                          value === "" ? null : Number.parseInt(value);
                         setNextService({
                           ...nextService,
-                          naiveHealthCheck: e.currentTarget.checked,
-                        })
-                      }
+                          stopTimeout:
+                            parsedValue === null
+                              ? null
+                              : Math.min(Math.max(0, parsedValue), 900),
+                        });
+                      }}
                     />
-                  </Tooltip>
+                  </FormGroup>
                 </div>
-                <div className="flex mt-4">
+              </div>
+              <div className="flex mt-4">
+                <Button
+                  name="settings-save"
+                  className="w-40 flex font-semibold"
+                  type="submit"
+                  disabled={!changesExist}
+                  isLoading={modifyLoader.isLoading}
+                >
+                  Save Changes
+                </Button>
+                {changesExist ? (
                   <Button
-                    name="autoscaling"
-                    className="w-40 flex font-semibold"
-                    type="submit"
-                    disabled={!changesExist}
-                    isLoading={modifyLoader.isLoading}
+                    className="w-40 ml-2 flex font-semibold"
+                    onClick={cancelChanges}
+                    variant="white"
                   >
-                    Save Changes
+                    Cancel
                   </Button>
-                  {changesExist ? (
-                    <Button
-                      className="w-40 ml-2 flex font-semibold"
-                      onClick={cancelChanges}
-                      variant="white"
-                    >
-                      Cancel
-                    </Button>
-                  ) : null}
-                </div>
-              </>
-            )}
+                ) : null}
+              </div>
+            </div>
           </div>
         </form>
       </Box>
