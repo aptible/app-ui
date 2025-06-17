ff6728fd8d6b5ed35b6e8288617d2b8e80f53df1
Eric Bower <me@erock.io>
2025-06-17 10:28:17 -0400

    add retry

--- src/ui/pages/signup.test.tsx
+++ src/ui/pages/signup.test.tsx
@@ -106,86 +106,106 @@ describe("Signup page", () => {
     ).toBeInTheDocument();
   });
 
-  it.skip("after successful signup, redirects to verify page", async () => {
-    server.use(
-      rest.get(`${testEnv.authUrl}/current_token`, (_, res, ctx) => {
-        return res(ctx.status(401));
-      }),
-    );
-    const { App } = setupAppIntegrationTest({
-      initEntries: [signupUrl()],
-    });
+  it(
+    "after successful signup, redirects to verify page",
+    { retry: 3 },
+    async () => {
+      expect.assertions(1);
+      server.use(
+        rest.get(`${testEnv.authUrl}/current_token`, (_, res, ctx) => {
+          return res(ctx.status(401));
+        }),
+      );
+      const { App } = setupAppIntegrationTest({
+        initEntries: [signupUrl()],
+      });
 
-    render(<App />);
+      render(<App />);
 
-    const name = await screen.findByRole("textbox", { name: "name" });
-    await act(() => userEvent.type(name, "mock name"));
-    const company = await screen.findByRole("textbox", { name: "company" });
-    await act(() => userEvent.type(company, "mock company"));
-    const email = await screen.findByRole("textbox", { name: "email" });
-    await act(() => userEvent.type(email, testEmail));
-    const pass = await screen.findByLabelText("password");
-    await act(() => userEvent.type(pass, "Aptible!1234"));
+      const name = await screen.findByRole("textbox", { name: "name" });
+      await act(() => userEvent.type(name, "mock name"));
+      const company = await screen.findByRole("textbox", { name: "company" });
+      await act(() => userEvent.type(company, "mock company"));
+      const email = await screen.findByRole("textbox", { name: "email" });
+      await act(() => userEvent.type(email, testEmail));
+      const pass = await screen.findByLabelText("password");
+      await act(() => userEvent.type(pass, "Aptible!1234"));
 
-    const btn = await screen.findByRole("button", { name: "Create Account" });
-    fireEvent.click(btn);
+      const btn = await screen.findByRole("button", { name: "Create Account" });
+      fireEvent.click(btn);
 
-    expect(await screen.findByText("Check your Email")).toBeInTheDocument();
-  });
+      expect(await screen.findByText("Check your Email")).toBeInTheDocument();
+    },
+  );
 
-  it("during signup expect validation to guard against submission", async () => {
-    server.use(
-      rest.get(`${testEnv.authUrl}/current_token`, (_, res, ctx) => {
-        return res(ctx.status(401), ctx.json({}));
-      }),
-    );
-    const { App } = setupAppIntegrationTest({
-      initEntries: [signupUrl()],
-    });
+  it(
+    "during signup expect validation to guard against submission",
+    { retry: 3 },
+    async () => {
+      expect.assertions(7);
+      server.use(
+        rest.get(`${testEnv.authUrl}/current_token`, (_, res, ctx) => {
+          return res(ctx.status(401), ctx.json({}));
+        }),
+      );
+      const { App } = setupAppIntegrationTest({
+        initEntries: [signupUrl()],
+      });
 
-    render(<App />);
+      render(<App />);
 
-    const btn = await screen.findByRole("button", { name: "Create Account" });
+      const btn = await screen.findByRole("button", { name: "Create Account" });
 
-    // disabled with no inputs set, does not redirect
-    expect(btn.textContent).toEqual("Create Account");
-    fireEvent.click(btn);
-    expect(await screen.findByText("Get started for free")).toBeInTheDocument();
+      // disabled with no inputs set, does not redirect
+      expect(btn.textContent).toEqual("Create Account");
+      fireEvent.click(btn);
+      expect(
+        await screen.findByText("Get started for free"),
+      ).toBeInTheDocument();
 
-    const name = await screen.findByRole("textbox", { name: "name" });
-    await act(() => userEvent.type(name, "mock name"));
+      const name = await screen.findByRole("textbox", { name: "name" });
+      await act(() => userEvent.type(name, "mock name"));
 
-    // will not redirect yet
-    fireEvent.click(btn);
-    expect(await screen.findByText("Get started for free")).toBeInTheDocument();
+      // will not redirect yet
+      fireEvent.click(btn);
+      expect(
+        await screen.findByText("Get started for free"),
+      ).toBeInTheDocument();
 
-    const company = await screen.findByRole("textbox", { name: "company" });
-    await act(() => userEvent.type(company, "mock company"));
+      const company = await screen.findByRole("textbox", { name: "company" });
+      await act(() => userEvent.type(company, "mock company"));
 
-    // will not redirect yet
-    fireEvent.click(btn);
-    expect(await screen.findByText("Get started for free")).toBeInTheDocument();
+      // will not redirect yet
+      fireEvent.click(btn);
+      expect(
+        await screen.findByText("Get started for free"),
+      ).toBeInTheDocument();
 
-    const email = await screen.findByRole("textbox", { name: "email" });
-    const pass = await screen.findByLabelText("password");
+      const email = await screen.findByRole("textbox", { name: "email" });
+      const pass = await screen.findByLabelText("password");
 
-    await act(() => userEvent.type(pass, "Aptible!1234"));
-    await act(() => userEvent.type(email, "invalid"));
-    fireEvent.click(btn);
-    expect(await screen.findByText("Get started for free")).toBeInTheDocument();
+      await act(() => userEvent.type(pass, "Aptible!1234"));
+      await act(() => userEvent.type(email, "invalid"));
+      fireEvent.click(btn);
+      expect(
+        await screen.findByText("Get started for free"),
+      ).toBeInTheDocument();
 
-    // set valid email
-    await act(() => userEvent.clear(pass));
-    await act(() => userEvent.type(email, testEmail));
+      // set valid email
+      await act(() => userEvent.clear(pass));
+      await act(() => userEvent.type(email, testEmail));
 
-    await act(() => userEvent.type(pass, "a"));
-    fireEvent.click(btn);
-    expect(await screen.findByText("Get started for free")).toBeInTheDocument();
+      await act(() => userEvent.type(pass, "a"));
+      fireEvent.click(btn);
+      expect(
+        await screen.findByText("Get started for free"),
+      ).toBeInTheDocument();
 
-    await act(() => userEvent.type(pass, "Aptible!1234"));
-    fireEvent.click(btn);
-    expect(await screen.findByText("Check your Email")).toBeInTheDocument();
-  });
+      await act(() => userEvent.type(pass, "Aptible!1234"));
+      fireEvent.click(btn);
+      expect(await screen.findByText("Check your Email")).toBeInTheDocument();
+    },
+  );
 
   it("errors properly when claim fails (check claim)", async () => {
     server.use(
